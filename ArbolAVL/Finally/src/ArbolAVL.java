6044100545beb95e0dc88ff28653712b71a9dd1e
emmanuel-rosillo <72666912+emmanuel-rosillo@users.noreply.github.com>
2022-05-09 18:50:51 -0500

    add 1° menu

--- ArbolAVL/Finally/src/ArbolAVL.java
+++ ArbolAVL/Finally/src/ArbolAVL.java
@@ -346,4 +346,8 @@ public class ArbolAVL extends ArbolBin {
         super.breadthFrist();
     }
 
+    @Override
+    public boolean inTree(Nodo search) {
+        return super.inTree(search);
+    }
 }
--- ArbolAVL/Finally/src/main.java
+++ ArbolAVL/Finally/src/main.java
@@ -20,16 +20,20 @@ public class main {
 //        a1.buildTree(n6);
 //        a1.breadthFrist();
 
+        // se ejecuta el menu, hasta que se ingrese la opcion 5
         while (true) {
-            Scanner n1 = new Scanner(System.in);
+            //menu de las opciones para el usuario
+            Scanner n1 = new Scanner(System.in); // objeto de lectura por teclado
             System.out.println("Seleccione una opción: \n");
             System.out.println("1.- plantar arbol\n");
-            System.out.println("2.- inserta ABONO (nodo)\n");
-            System.out.println("3.- Cortar parte del arbol (nodo)\n");
-            Integer op = n1.nextInt();
-            Boolean frue = false;
+            System.out.println("2.- inserta ABONO (añadir nodo)\n");
+            System.out.println("3.- Cortar parte del arbol (borrar nodo)\n");
+            System.out.println("4.- buscar valor en el arbol");
+            Integer op = n1.nextInt(); // opcion elegida
+            Boolean frue = false;// validar si la raiz fue insertada op "1"
             ArbolAVL a1 = null;
             switch (op) {
+                //insertar raiz
                 case 1:
                     if(frue == true){
                         System.err.println("no es posible insertar nuevamente una raiz");
@@ -42,18 +46,46 @@ public class main {
                     frue = true;
                     break;
                 case 2:
+                    //añadir nodos al arbol
                     if(frue = true){
-                        System.out.println("Ingresa la raiz: ");
+                        System.out.println("Ingresa el nodo: ");
                         Integer y = n1.nextInt();
                         Nodo n = new Nodo(y);
                         a1.buildTree(n);
                     }else {
                         System.err.println("No has ingresado una raiz");
                     }
+                    break;
                 case 3:
-
+                    // borrar nodo
+                    System.out.println("ingresa a quien matamos:");
+                    Integer z = n1.nextInt();
+                    Nodo d = new Nodo(z);
+                    a1.deleteKey(d);
                 case 4:
-                case 5:
+                    // buscar nodo
+                    System.out.println("diganme ¿a quien localizamos?: ");
+                    Integer h = n1.nextInt();
+                    Nodo l = new Nodo(h);
+                    Boolean tr = a1.inTree(l);
+                    if (tr == true){
+                        System.out.println("Ya lo encontramos vivo");
+                    }
+                    else {
+                        System.err.println("No encontramos rastros de vida");
+                    }
+            }
+            // finalizar programa
+            if (op == 5){
+                System.out.println("Saliendo del programa...");
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                finally {
+                    break;
+                }
             }
         }
     }
